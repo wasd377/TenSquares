61c1812b7ed747163ceacf80d673f38e450a127c
Антон <anton@workout.su>
2024-02-29 13:19:01 +0300

    Initial Commit

---
+++ TenSquares/TenSquaresApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenSquaresApp.swift
+//  TenSquares
+//
+//  Created by Natalia D on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TenSquaresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
