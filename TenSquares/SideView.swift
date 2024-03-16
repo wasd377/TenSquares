32fbfd7e71543d60a3526dda2656872c71bad35e
Антон <anton@workout.su>
2024-03-16 11:06:20 +0300

    Исправлен баг с тем, что не все квадратики помещались на экране.

--- TenSquares/SideView.swift
+++ TenSquares/SideView.swift
@@ -14,33 +14,85 @@ struct SideView: View {
     var side: String
     @State var text = "test"
     
-    var body: some View {
-        if side == "Master" {
-            HStack{
+    struct CardView: View {
+        var body: some View {
+            GeometryReader { gp in
                 VStack {
-                    Text("Master")
-                    ForEach(vm.masterSquares, id: \.id) { square in
-                        
-                        SquareView(color: square.color, id: square.id, side: "Master")
-                        
+                    VStack {
+                        Text("Blue")
                     }
+                    .frame(width: gp.size.width, height: gp.size.height * 0.7)
+                    .background(Color.blue)
+                    VStack {
+                        Text("Red")
+                    }
+                    .frame(width: gp.size.width, height: gp.size.height * 0.3)
+                    .background(Color.red)
                 }
-    
             }
+            .frame(height: 280).frame(maxWidth: .infinity)
+            .cornerRadius(24).padding(.horizontal, 30)
         }
-        if side == "Player" {
-            HStack{
-                VStack {
-                    Text("Player")
-                    ForEach(vm.playerSquares, id: \.self) { square in
-                        SquareView(color: square.color, id: square.id, side: "Player")
-                        }
-                    
-                }
+    }
+    
+    var body: some View {
+        
+            if side == "Master" {
               
-    
+                    VStack {
+                        Text("Master")
+                        VStack{
+                            HStack {
+                                ForEach(vm.masterSquares, id: \.self) { square in
+                                    if square.id <= 5 {
+                                        SquareView(color: square.color, id: square.id, side: "Master")
+                                    }
+                                }
+                            }
+                            HStack {
+                                ForEach(vm.masterSquares, id: \.self) { square in
+                                    if square.id > 5 {
+                                        SquareView(color: square.color, id: square.id, side: "Master")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                
             }
-        }
+            if side == "Player" {
+                
+            
+                    VStack {
+                        Text("Player")
+                        
+                            VStack{
+                                HStack {
+                                    ForEach(vm.playerSquares, id: \.self) { square in
+                                        if square.id <= 5 {
+                                            SquareView(color: square.color, id: square.id, side: "Player")
+                                        }
+                                    }
+                                }
+                                HStack {
+                                    ForEach(vm.playerSquares, id: \.self) { square in
+                                        if square.id > 5 {
+                                            SquareView(color: square.color, id: square.id, side: "Player")
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                       
+                        
+                   
+                    
+                    
+                
+            }
+        
+        
     }
 }
 
